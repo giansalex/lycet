eaf15ed00071bc0ba4341ffaa7260d99e13a31cd
giansalex <giansalex@gmail.com>
2018-01-23 16:38:43 -0500

    add unittest

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # LYCET - Greenter
+[![Travis-CI](https://img.shields.io/travis/giansalex/lycet.svg?branch=master&style=flat-square)](https://travis-ci.org/giansalex/lycet)
 
 Lycet is a REST API based on [greenter](https://github.com/giansalex/greenter) and use Symfony Framework.
 
---
+++ tests/Controller/HomeControllerTest.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrador
+ * Date: 23/01/2018
+ * Time: 03:48 PM
+ */
+
+namespace App\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class HomeControllerTest extends WebTestCase
+{
+    public function testIndex()
+    {
+        $client = static::createClient();
+
+        $client->request('GET', '/');
+
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+    }
+}
