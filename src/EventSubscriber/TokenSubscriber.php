fc07c760d43aad05f11b60a54108de415c367c8b
Giancarlos Salas <giansalex@gmail.com>
2018-02-17 20:28:28 -0500

    add basic token authentication

---
+++ src/EventSubscriber/TokenSubscriber.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Giansalex
+ * Date: 17/02/2018
+ * Time: 20:02
+ */
+
+namespace App\EventSubscriber;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+class TokenSubscriber implements EventSubscriberInterface
+{
+    /**
+     * @var string
+     */
+    private $token;
+
+    public function __construct($token)
+    {
+        $this->token = $token;
+    }
+
+    public function onKernelRequest(GetResponseEvent $event)
+    {
+        $path = $event->getRequest()->getPathInfo();
+
+        $isApi = substr($path, 0, 4) === "/api";
+
+        if ($isApi) {
+            $token = $event->getRequest()->query->get('token');
+            if ($token !== $this->token) {
+                throw new AccessDeniedHttpException('This action needs a valid token!');
+            }
+        }
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return array(
+            KernelEvents::REQUEST => 'onKernelRequest',
+        );
+    }
+}
