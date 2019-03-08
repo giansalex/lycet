3b7e52d5e3e7980229293876b44ab4983dc27d3b
Giancarlos Salas <giansalex@gmail.com>
2019-03-08 15:21:58 -0500

    optimize symfony packages

--- config/bundles.php
+++ config/bundles.php
@@ -3,6 +3,5 @@
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
-    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
 ];
